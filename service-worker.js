d2a17abe7af49b6a529aeac7edad661c0513a2b0
yc-0 <y-chiba@winc.co.jp>
2019-07-12 13:15:45 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -24,3 +24,30 @@ self.addEventListener('fetch', (event) => {
       })
   );
 });
+
+self.addEventListener("push", function(event) {
+  event.waitUntil(
+    self.registration.pushManager.getSubscription()
+    .then(function(subscription) {
+      if (subscription) {
+        return sbscription.endpoint;
+      } 
+      throw new Error('User not subscribed');
+    })
+    .then(function(res) {
+      return fetch('/notification.json');
+    })
+    .then(function(res) {
+      if (res.status === 200) {
+        return res.json();
+      }
+      throw new Error('notification api response error');
+    })
+    .then(function(res) {
+      return self.registration.showNotification(res.title, {
+        icon: '/images/icon.png',
+        body: res.body
+      })
+    })
+  )
+});
