29311be66b27bbd09c8cb2480b0f37c49bc2b5db
yc-0 <y-chiba@winc.co.jp>
2019-07-12 14:32:35 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -30,7 +30,7 @@ self.addEventListener("push", function(event) {
     self.registration.pushManager.getSubscription()
     .then(function(subscription) {
       if (subscription) {
-        return sbscription.endpoint;
+        return subscription.endpoint;
       } 
       throw new Error('User not subscribed');
     })
